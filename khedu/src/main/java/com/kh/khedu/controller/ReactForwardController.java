9b77704ed44fbdbf34ad0dfbd1573b0895aa53b2
tookwak5-sudo <tookwak5@gmail.com>
2026-09-21 00:54:15 +0900

    배포테스트10

--- khedu/src/main/java/com/kh/khedu/controller/ReactForwardController.java
+++ khedu/src/main/java/com/kh/khedu/controller/ReactForwardController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReactForwardController {
 	@GetMapping("/**")
 	public String forward() {
-		
 		return "forward:/index.html";
 	}
 }
